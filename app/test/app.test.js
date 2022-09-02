5d1f0d5781e784cd63c260f0124df49964d1d9f9
Sam Devorsetz <samdevorsetz@gmail.com>
2022-09-02 15:05:12 -0400

    remove gae

--- app/test/app.test.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-const supertest = require('supertest');
-const app = require(path.join(__dirname, '../', 'app.js'));
-
-describe('gae_flex_node_static_files', () => {
-  it('should be listening', async () => {
-    await supertest(app).get('/').expect(200);
-  });
-});
